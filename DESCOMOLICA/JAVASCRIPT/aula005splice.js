958e6e6e01975782ad2722b4be6da4411619eeff
PauloC <soci4lmidi4@gmail.com>
2024-01-11 18:44:25 -0300

    aula005splice

---
+++ DESCOMOLICA/JAVASCRIPT/aula005splice.js
@@ -0,0 +1,16 @@
+//Retirando elementos de um Array com "splice()"
+var arr = [1, 2, 3, 4, 5, 6];
+
+arr.splice(2, 1);
+console.log(arr);
+
+//Retirando elementos e subistituindo por outro usando o "splice()"
+console.log("Retirando os elementos das posições 1 e 2, e trocando por outros elementos!");
+var nome = ["Paulo", "Vitoria","Dandara","Isadora"];
+console.log(nome);
+
+console.log("Elementos retirados!");
+var novos = nome.splice(1, 2, "Thayanne", "Joaquim");
+
+console.log(novos);
+console.log(nome);
