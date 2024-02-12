4afc97c2803d29558e99304960add06bf530087b
PauloC <soci4lmidi4@gmail.com>
2024-02-12 15:17:25 -0300

    switch

---
+++ DESCOMOLICA/JAVASCRIPT/exercicios/INSTRUCOES/switch.js
@@ -0,0 +1,28 @@
+var diaSem = 9;
+
+switch(diaSem)
+{
+    case 1:
+        console.log("DOMINGO");
+        break;
+    case 2:
+        console.log("SEGUNDA");
+        break;
+    case 3:
+        console.log("TERÇA");
+        break;
+    case 4:
+        console.log("QUARTA");
+        break;
+    case 5:
+        console.log("QUINTA");
+        break;
+    case 6:
+        console.log("SEXTA");
+        break;
+    case 7:
+        console.log("SABADO");
+        break;
+    default:
+        console.log("Esse dia não existe!!");
+};
