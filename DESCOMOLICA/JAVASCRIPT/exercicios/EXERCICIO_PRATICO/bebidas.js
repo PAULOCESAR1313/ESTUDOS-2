96efb68ef3911968a1d748e8b8ecc9464bc656cc
PauloC <soci4lmidi4@gmail.com>
2024-02-12 20:00:04 -0300

    Pratica

---
+++ DESCOMOLICA/JAVASCRIPT/exercicios/EXERCICIO_PRATICO/bebidas.js
@@ -0,0 +1,25 @@
+var bebidas = 7;
+
+switch(bebidas)
+{
+    case 1:
+        console.log("REFRIGERANTE COCACOLA 1L:\n" + "Valor:\n" + "R$ 8,00");
+        break;
+    case 2:
+        console.log("REFRIGERANTE COCACOLA 2L:\n" + "Valor:\n" + "R$ 10,00");
+        break;
+    case 3:
+        console.log("REFRIGERANTE FANTA 1L:\n" + "Valor:\n" + "R$ 8,00");
+        break;
+    case 4:
+        console.log("REFRIGERANTE FANTA 2L:\n" + "Valor:\n" + "R$ 10,00");
+        break;
+    case 5:
+        console.log("REFRIGERANTE ANTARTICA 1L:\n" + "Valor:\n" + "R$ 8,00");
+        break;
+    case 6:
+        console.log("REFRIGERANTE ANTARTICA 2L:\n" + "Valor:\n" + "R$ 10,00");
+        break;
+    default:
+        console.log("Não temos esse produto!!");
+};
