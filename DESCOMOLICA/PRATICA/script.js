a24321ed59cda9ddf7324062ab591b6c475dd1a6
PauloC <soci4lmidi4@gmail.com>
2024-02-11 22:37:26 -0300

    Update script.js

--- DESCOMOLICA/PRATICA/script.js
+++ DESCOMOLICA/PRATICA/script.js
@@ -2,5 +2,6 @@ let pratc = require('readline-sync');
 
 let nome = pratc.question("Digite seu primeiro nome\n");
 let sobreNome = pratc.question("Digite o seu sobrenome:\n");
+let nomeComplet = `Olá, ${nome} ${sobreNome}`;
 
-console.log(`Olá, ${nome}' '${sobreNome}`);
+console.log(nomeComplet);
