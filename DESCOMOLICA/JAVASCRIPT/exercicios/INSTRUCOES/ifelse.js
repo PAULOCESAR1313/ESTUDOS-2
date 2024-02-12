9c2453d9f7d4c8001c0a8daebf92ec1935deec24
PauloC <soci4lmidi4@gmail.com>
2024-02-12 15:03:48 -0300

    a

---
+++ DESCOMOLICA/JAVASCRIPT/exercicios/INSTRUCOES/ifelse.js
@@ -0,0 +1,25 @@
+var pais = "Argrntina";
+
+if(pais != "Brasil")
+{
+    console.log("Você é estrangeiro!!");
+}
+else 
+{ 
+    console.log("Você é brasileiro!!");
+}
+
+var idade = 20;
+
+if (idade < 16)
+{
+    console.log("Você não vota!!");
+}
+else if (idade < 18 || idade > 65)
+{
+    console.log("Seu voto é Opcional!!");
+}
+else 
+{
+    console.log("Seu voto é Obrigatorio!!");
+}
