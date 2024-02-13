09f63e7829c1d8572af5b75ec0af6ad0a9dbcc14
PauloC <soci4lmidi4@gmail.com>
2024-02-12 21:27:45 -0300

    fro

---
+++ DESCOMOLICA/JAVASCRIPT/exercicios/MANIPULANDO_ARRAY/node_3d6b4acbdc7d5.tmp.js
@@ -0,0 +1,19 @@
+const carros = 
+[
+    { id: 1, modelo: "Corsa", marca: "Chevrolet", preco: 45000, dataCriacao: "01/01/2011"},
+  
+    { id: 2, modelo: "Punta", marca: "Fiat", preco: 35000, dataCriacao: "01/02/2022"},
+  
+    { id: 3, modelo: "Gol", marca: "Volkswagen", preco: 40000, dataCriacao: "01/03/2023"}
+  
+];
+
+let totalPreco = 0;
+
+for(let i = 0; i < carros.length; i)
+{
+    const carro = carros[i];
+    totalPreco += carro.preco;
+}
+
+console.log("Total de Preços dos carros: ", totalPreco);
---
+++ DESCOMOLICA/JAVASCRIPT/exercicios/WHILE_DO_WHILE/while.js
@@ -0,0 +1,18 @@
+console.log("While");
+
+var c = 1;
+
+while (c <= 10)
+{
+    console.log(c);
+    c++
+}
+
+console.log("Do While")
+
+var t = 10;
+do
+{
+    console.log(t);
+    t--
+}while(t == 1)
