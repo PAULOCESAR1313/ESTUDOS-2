f388c115ab421d03fb97ca258178c228f24dccbc
PauloC <soci4lmidi4@gmail.com>
2024-02-13 12:43:43 -0300

    string

---
+++ DESCOMOLICA/JAVASCRIPT/exercicios/STRINGS/string0001.js
@@ -0,0 +1,7 @@
+//Método para pegar o tamanho de uma string
+var nome = "Paulo César";
+var tString = nome.length;
+
+resultado = tString > 8 ? "Obrigado!" : "Por favor preencha seu nome completo"
+
+console.log(resultado);
