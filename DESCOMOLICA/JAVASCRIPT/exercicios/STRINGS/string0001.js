66423c499238d5133238e74f3bd35ae0cabc4f71
PauloC <soci4lmidi4@gmail.com>
2024-02-13 18:02:45 -0300

    al strigs

--- DESCOMOLICA/JAVASCRIPT/exercicios/STRINGS/string0001.js
+++ DESCOMOLICA/JAVASCRIPT/exercicios/STRINGS/string0001.js
@@ -2,6 +2,18 @@
 var nome = "Paulo César";
 var tString = nome.length;
 
+console.log(tString);
 resultado = tString > 8 ? "Obrigado!" : "Por favor preencha seu nome completo"
 
 console.log(resultado);
+
+var caixaAl = nome.toUpperCase();
+console.log(caixaAl);
+
+var jogo = "Barcelona vs Real o melhor time!"
+var cortado = jogo.indexOf("Real");
+console.log(cortado);
+
+var cortado = jogo.slice(13,);
+console.log(cortado);
+
