daa7907602858939f7e39fac06b2536ace2eae1a
Paulo César <soci4lmidi4@gmail.com>
2024-03-10 06:53:24 -0300

    MANIPULANDO O DOM
    TRÊS FORMAS DE MANIPULAR O DOM

---
+++ DESCOMOLICA/JAVASCRIPT/exercicios/MANIPULANDO O DOM/script.js
@@ -0,0 +1,14 @@
+/*
+var nun1 = Number.parseInt(window.prompt("Digite um número!"));
+var nun2 = Number.parseInt(window.prompt("Digite outro número!"));
+
+var calc = nun1 + nun2;
+var resultado = calc;
+
+window.alert("O resultado é: " + resultado);
+*/
+
+var nome = window.prompt("Digite seu nome:");
+var nome = window.prompt("Digite seu sobrenome:");
+
+window.alert("Seu nome completo é: " + nome + " " + sobrenome);
